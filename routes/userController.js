f32e2f1a9b1dfa7d854815b08c0c9c74b3ebe254
bethschoenfeld <bethingraham3@gmail.com>
2018-01-23 11:02:35 -0500

    setting up the heroku app

---
+++ routes/userController.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const User = require('../db/models/User')
+const router = express.Router()
+
+router.get('/', async (req, res) => {
+    try {
+        const users = await User.find({})
+        res.json(users)
+    } catch (err) {
+        console.log(err)
+        res.sendStatus(500)
+    }
+})
+
+router.post('/', async (req, res) => {
+    try {
+        const newUser = await User.create({})
+        res.json(newUser)
+    } catch (err) {
+        console.log(err)
+        res.sendStatus(500)
+    }
+})
+
+router.delete('/:userId', async (req, res) => {
+    try {
+        await User.findByIdeAndRemove(req.params.userId)
+        res.sendStatus(200)
+    } catch (err) {
+        console.log(err)
+        res.sendStatus(500)
+    }
+})
+
+router.patch('/:userId', async (req, res) => {
+    try {
+        const updatedIdea = await User.findByIdeAndUpdate(req.params.userId, req.body, { new: true })
+        res.json(updateIdea)
+    } catch (err) {
+        console.log(err)
+        res.sendStatus(500)
+    }
+})
+
+module.exports = router
