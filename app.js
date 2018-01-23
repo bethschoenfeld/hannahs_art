396f2f22f0a901275b4a593dfb88efd7238eb32d
bethschoenfeld <bethingraham3@gmail.com>
2018-01-23 10:57:17 -0500

    Deleting unnecessary views and setting up app.js with mongoose .env and controllers

--- app.js
+++ app.js
@@ -1,46 +1,69 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+require("dotenv").config() //put this in the seeds file too
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+const express = require('express')
+const path = require('path')
+const favicon = require('serve-favicon')
+const logger = require('morgan')
+const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 
-var app = express();
+const app = express()
+
+//mongo setup
+mongoose.Promise = global.Promise
+mongoose.connect(process.env.MONGODB_URI)
+
+const connection = mongoose.connection
+connection.on('connected', () => {
+  console.log('Mongoose Connection Successfully')
+})
+connection.on('error', (err) => {
+  console.log('Mongoose default connection error: ' + err)
+})
+
+//serve static react files
+app.use(express.static(__dirname + '/client/build/'));
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/client/build/index.html')
+})
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'hbs')
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/', index);
-app.use('/users', users);
+// Controllers
+const userController = require('./routes/userController.js')
+app.use('/api/users', userController)
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+app.use(function (req, res, next) {
+  const err = new Error('Not Found')
+  err.status = 404
+  next(err)
+})
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.message = err.message
+  res.locals.error = req
+    .app
+    .get('env') === 'development'
+    ? err
+    : {}
 
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+  res.status(err.status || 500)
+  res.render('error')
+})
 
-module.exports = app;
+module.exports = app
