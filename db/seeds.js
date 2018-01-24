a36ba832228aff40a5c6e74c197f9af20ed2f11c
bethschoenfeld <bethingraham3@gmail.com>
2018-01-23 21:26:18 -0500

    it was working. I don't know what's happening

--- db/seeds.js
+++ db/seeds.js
@@ -1,11 +1,11 @@
 require('dotenv').config()
 
-const mongoose = require('mongoose')
-mongoose.connect(process.env.MONGODB_URI)
-
 const User = require('./models/User.js')
 const Transaction = require('./models/Transaction.js')
 const Product = require('./models/Product.js')
+
+const mongoose = require('mongoose')
+mongoose.connect(process.env.MONGODB_URI)
 
 mongoose.connection.once('open', () => {
     console.log(`Mongoose has connected to MongoDB`)
