20924564c81b4efccc17c0d8e932c3f185baacf7
bethschoenfeld <bethingraham3@gmail.com>
2018-01-28 22:43:46 -0500

    preliminary styling

--- client/src/components/UserList.js
+++ client/src/components/UserList.js
@@ -1,84 +1,82 @@
 import React, { Component } from 'react'
-import styled from 'styled-components'
-import { connect } from 'react-redux'
-import { push } from 'react-router-redux'
-import { getUserRoute, deleteUserFromDatabase } from '../actions/thunk.js'
-import EditUserForm from './EditUserForm'
+    import styled from 'styled-components'
+    import { connect } from 'react-redux'
+    import { push } from 'react-router-redux'
+    import { getUserRoute, deleteUserFromDatabase } from '../actions/thunk.js'
+    import EditUserForm from './EditUserForm'
 
 const UserListWrapper = styled.div`
     border: solid green 1px;
-    `
-const UserWrapper = styled.div`
-    border: 1px solid cyan;
-    button{
-    background-color: #e7e7e7;
-    color: black;
-    border: 2px solid #e7e7e7;
+    .userWrapper{
+        border: 1px solid cyan;
+        button{
+        background-color: #e7e7e7;
+        color: black;
+        border: 2px solid #e7e7e7;
+}
 }
 `
-
 class UserList extends Component {
-
-  componentWillMount() {
-    this.props.getUserRoute()
-  }
-
-  render() {
-    return (
-      < UserListWrapper >
-        {this.props.users.map((user, i) => {
-          return (
-            <UserWrapper key={i}>
-              <div className="">
-                {user.username}
-              </div>
-              <button onClick={() => this.props.deleteUserFromDatabase(user)}>
-                Delete
-              </button>
-              <EditUserForm user={user}/>
-              <div className="">
-                {user.contact.map((contact, i)=>{
-                  return(
-                    <div key={i}>
-                      <div className="">
-                        Contact
-                      </div>
-                      <div className="">
-                        {contact.email}
-                      </div>
-                      <div className="">
-                        {contact.instagram}
-                      </div>
-                      <div className="">
-                        {contact.website}
-                      </div>
-                      <div className="">
-                        {contact.artwork.map((artwork, i)=>{
-                          return(
-                            <div key={i}>
-                              <div className="">
-                                Artwork
-                              </div>
-                              <div className="">
-                                <img height="200px" src={artwork.artwork} alt="artwork"/>
-                              </div>
+    componentWillMount() {
+        this.props.getUserRoute()
+    }
+    render() {
+        return (
+            < UserListWrapper >
+                {this.props.users.map((user, i) => {
+                    return (
+                        <div className="userWrapper" key={i}>
+                            <div className="">
+                                {user.username}
                             </div>
-                          )
-                        })}
-                      </div>
-                    </div>
-                  )
-                })}
-              </div>
-            </UserWrapper>
-          )
-        })
-        }
-      </UserListWrapper >
-    )
-  }
+                            <button onClick={() => this.props.deleteUserFromDatabase(user)}>
+                                Delete
+                            </button>
+                            <EditUserForm user={user} />
+                            <div className="">
+                                {user.contact.map((contact, i) => {
+                                    return (
+                                        <div key={i}>
+                                            <div className="">
+                                                Contact
+                                            </div>
+                                            <div className="">
+                                                {contact.email}
+                                            </div>
+                                            <div className="">
+                                                {contact.instagram}
+                                            </div>
+                                            <div className="">
+                                                {contact.website}
+                                            </div>
+                                            <div className="">
+                                                {contact.artwork.map((artwork, i) => {
+                                                    return (
+                                                        <div key={i}>
+                                                            <div className="">
+                                                                Artwork
+                                                            </div>
+                                                            <div className="">
+                                                                <img height="200px" src={artwork.artwork} alt="artwork" />
+                                                            </div>
+                                                        </div>
+                                                    )
+                                                })}
+                                            </div>
+                                        </div>
+                                    )
+                                })}
+                            </div>
+                        </div>
+                    )
+                })
+                }
+            </UserListWrapper >
+        )
+    }
 }
+
 const mapStateToProps = (state) => {
-  return { users: state.users }
+    return { users: state.users }
 }
 export default connect(mapStateToProps, { push, getUserRoute, deleteUserFromDatabase })(UserList)
