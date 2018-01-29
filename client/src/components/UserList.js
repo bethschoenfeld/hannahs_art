351700b76a031f6a15cde226cd80944f1acf1822
bethschoenfeld <bethingraham3@gmail.com>
2018-01-28 22:36:44 -0500

    Got all models up and running.

--- client/src/components/UserList.js
+++ client/src/components/UserList.js
@@ -29,13 +29,47 @@ class UserList extends Component {
         {this.props.users.map((user, i) => {
           return (
             <UserWrapper key={i}>
-              <div>
+              <div className="">
                 {user.username}
               </div>
               <button onClick={() => this.props.deleteUserFromDatabase(user)}>
                 Delete
               </button>
               <EditUserForm user={user}/>
+              <div className="">
+                {user.contact.map((contact, i)=>{
+                  return(
+                    <div key={i}>
+                      <div className="">
+                        Contact
+                      </div>
+                      <div className="">
+                        {contact.email}
+                      </div>
+                      <div className="">
+                        {contact.instagram}
+                      </div>
+                      <div className="">
+                        {contact.website}
+                      </div>
+                      <div className="">
+                        {contact.artwork.map((artwork, i)=>{
+                          return(
+                            <div key={i}>
+                              <div className="">
+                                Artwork
+                              </div>
+                              <div className="">
+                                <img height="200px" src={artwork.artwork} alt="artwork"/>
+                              </div>
+                            </div>
+                          )
+                        })}
+                      </div>
+                    </div>
+                  )
+                })}
+              </div>
             </UserWrapper>
           )
         })
